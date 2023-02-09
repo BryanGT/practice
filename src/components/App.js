94977b7d8915b51bbf5894aeaca98351125dd317
BryanGT <sanbsbr@gmail.com>
2023-02-08 21:04:07 -0600

    feat(core): created app Coffee Shop

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../containers/Layout.jsx';
+import Header from './header';
+import '../styles/main.scss';
+
+const App = () => {
+    return (
+        <Layout>
+            <Header></Header>
+        </Layout>
+    );
+};
+
+export default App;
---
+++ src/components/header/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './style.scss'
+
+function Header() {
+  return (
+    <>
+      <header className='header'>
+        <div className="header-nav">
+          <a href="#home" className="header-nav__title">The Coffee Shop</a>
+
+          <ul className="header-nav__wrapper">
+            <li className="header-nav__item"><a href="#contact">Contact</a></li>
+            <li className="header-nav__item"><a href="#about">About</a></li>
+          </ul>
+          <a className="icon header-nav__icon">
+            <i className="fa fa-bars"></i>
+          </a>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
