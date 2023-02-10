63582bed4886443522ab6997aeba8708bbf72e62
BryanGT <sanbsbr@gmail.com>
2023-02-09 23:21:37 -0600

    feat(excercise2): Created file with script

---
+++ src/assets/js/excercise2.js
@@ -0,0 +1,6 @@
+(() => {
+  /**
+   * Here the script to run in the browser Console
+   * This works on this page https://littlesleepies.com/products/rust-rainbows-bamboo-viscose-zippy
+   */
+})();
