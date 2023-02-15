dbd644f2c60092be40ef330425a4e0276abbdeaa
BryanGT <sanbsbr@gmail.com>
2023-02-14 20:09:30 -0600

    feat(excercise3): completed

--- src/routes/App.js
+++ src/routes/App.js
@@ -4,6 +4,7 @@ import Layout from '../containers/Layout';
 import Landing from '../pages/landing';
 import NotFound from '../pages/notFound';
 import BaristaTransition from '../pages/barista-transition';
+import Form from '../pages/form';
 import SurveyQ2 from '../pages/surveyQ2'; 
 import '../styles/main.scss';
 import Results from '../pages/results';
@@ -17,6 +18,7 @@ const App = () => {
                     <Route path="/barista-transition" element={<BaristaTransition/>} />
                     <Route path="/survey-q2" element={<SurveyQ2/>}/>
                     <Route path="/results" element={<Results/>}/>
+                    <Route path="/csv" element={<Form/>}/>
                     <Route path="*" element={<NotFound/>}/>    
                 </Routes>
             </Layout>
